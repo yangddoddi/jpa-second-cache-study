8aeebbc5268833fce9bac953a983d0f6742ca734
yangddoddi <pud1251@gmail.com>
2023-03-22 21:57:32 +0900

    creat entity repository

---
+++ src/main/java/com/example/second_cache_study/infrastructure/HeaderEntityRepository.java
@@ -0,0 +1,6 @@
+package com.example.second_cache_study.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeaderEntityRepository extends JpaRepository<HeaderEntity, Long> {
+}
---
+++ src/main/java/com/example/second_cache_study/infrastructure/NodeEntityRepository.java
@@ -0,0 +1,6 @@
+package com.example.second_cache_study.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeEntityRepository extends JpaRepository<NodeEntity, Long> {
+}
