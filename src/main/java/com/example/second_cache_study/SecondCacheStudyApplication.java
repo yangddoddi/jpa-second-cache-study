2c0ce2603614e1578e2ee5b682fdbfefc9325285
yangddoddi <pud1251@gmail.com>
2023-03-22 21:46:24 +0900

    init

---
+++ src/main/java/com/example/second_cache_study/SecondCacheStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_cache_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondCacheStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondCacheStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/second_cache_study/SecondCacheStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_cache_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondCacheStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
